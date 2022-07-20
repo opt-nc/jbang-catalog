28d6de051cd73c534c995dea516d60d28b0a1fa2
Michèle BARRE <michele.barre@opt.nc>
2022-07-20 11:14:17 +1100

    add asci art

--- hello-optnc/HelloOptNc.java
+++ hello-optnc/HelloOptNc.java
@@ -6,6 +6,10 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 import java.util.concurrent.Callable;
 
 @Command(name = "hello-optnc", mixinStandardHelpOptions = true, version = "hello-optnc 0.1",
@@ -22,7 +26,38 @@ class HelloOptNc implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception { // your business logic goes here...
-        System.out.println("Hello World " + greeting);
+        printAsciiArt("Hello World " + greeting);
         return 0;
+    }
+
+    public static void printAsciiArt(String text) throws IOException {
+
+        int width = 200;
+        int height = 30;
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("SansSerif", Font.BOLD, 12));
+
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.drawString(text, 10, 20);
+
+        for (int y = 0; y < height; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < width; x++) {
+
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : "#");
+
+            }
+
+            if (sb.toString().trim().isEmpty()) {
+                continue;
+            }
+
+            System.out.println(sb);
+        }
+
     }
 }
