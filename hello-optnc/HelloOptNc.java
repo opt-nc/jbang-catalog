dff49ca001ac81dce3a9ad80cc46bda3bb55100a
Michèle BARRE <michele.barre@opt.nc>
2022-07-22 09:28:47 +1100

    chore: change police size

--- hello-optnc/HelloOptNc.java
+++ hello-optnc/HelloOptNc.java
@@ -37,18 +37,18 @@ class HelloOptNc implements Callable<Integer> {
 
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics g = image.getGraphics();
-        g.setFont(new Font("SansSerif", Font.BOLD, 12));
+        g.setFont(new Font("Serif", Font.BOLD, 12));
 
         Graphics2D graphics = (Graphics2D) g;
         graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
                 RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics.drawString(text, 10, 20);
+        graphics.drawString(text, 0, 10);
 
         for (int y = 0; y < height; y++) {
             StringBuilder sb = new StringBuilder();
             for (int x = 0; x < width; x++) {
 
-                sb.append(image.getRGB(x, y) == -16777216 ? " " : "#");
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : "o");
 
             }
 
